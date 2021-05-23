7ed90247bf063c1f2448f815d5f779b13985d5f9
LJLee37 <ljlee3759@gmail.com>
2021-05-23 17:29:21 +0900

    Edit build.sh dockerfile

--- buildLjleede.sh
+++ buildLjleede.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+docker buildx create --use
 if [[ "$#" -eq 0 ]]
 then
-    docker build -t ljlee37/ljleede:latest ljleede
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t ljlee37/ljleede:latest ljleede --pull --push
 elif [[ "$#" -eq 1 ]]
 then
-    docker build -t ljlee37/ljleede:$1 -t ljlee37/ljleede:latest ljleede
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t ljlee37/ljleede:$1 -t ljlee37/ljleede:latest ljleede --pull --push
 fi
-docker image push -a ljlee37/ljleede
